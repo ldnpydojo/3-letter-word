fa2906916d6eedaa0efe25f84b9c488f36ff310d
Keats <balthek@gmail.com>
2014-07-03 21:13:11 +0100

    Add a way to give up

--- words.py
+++ words.py
@@ -21,8 +21,28 @@ with open("CSW07.txt") as f:
 			print "%s,%s" % (word, definition)
 """
 
+def ask_user(picked_word):
+  guess = raw_input("> ")
+
+  if guess == "LOSER":
+    print "The picked word was %s" % picked_word
+    return True
+
+  if len(guess) != 3:
+    print "You need to enter a 3 letters word"
+    return False
+
+  score = comparison(guess, picked_word)
+  if score < 3:
+    print "You've got a score of %d" % score
+    return False
+
+  print "You win !"
+  return True
+
+
+
 if __name__ == '__main__':
-    
 
    with open("words.txt") as f:
        data = f.read().split('\n')
@@ -30,4 +50,18 @@ if __name__ == '__main__':
        word_end = selected_word.find(',')
        word = selected_word[:word_end]
        definition =  selected_word[word_end:]
+       print "Type LOSER if you want to give up"
+       print word, definition
+
+       tries = 0
+       while True:
+        won = ask_user(word)
+        if won:
+          print "Game over"
+          break
+
+        tries += 1
+        if tries > 10:
+          print "A little help..."
+          print "The word means: %s" % definition
 
