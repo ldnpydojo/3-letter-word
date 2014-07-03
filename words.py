a84e3450b14933d60039bb648a7d08856a5e22ed
Raúl Cumplido <raulcumplido@gmail.com>
2014-07-03 20:55:41 +0100

    team2 commit

--- words.py
+++ words.py
@@ -1,4 +1,13 @@
+import random
 
+def comparison(enter_word, selected_word):
+    score = 0
+    for x in range(len(enter_word)):
+        if enter_word[x] == selected_word[x]:
+            score += 1
+    return score
+
+"""
 with open("CSW07.txt") as f:
 	data = f.read().split('\n')
 	for line in data:
@@ -10,3 +19,15 @@ with open("CSW07.txt") as f:
 
 		if len(word) == 3:
 			print "%s,%s" % (word, definition)
+"""
+
+if __name__ == '__main__':
+    
+
+   with open("words.txt") as f:
+       data = f.read().split('\n')
+       selected_word = random.choice(data)
+       word_end = selected_word.find(',')
+       word = selected_word[:word_end]
+       definition =  selected_word[word_end:]
+
