70057038fa5f12e2a9fdfd7e407f35a0524e0f66
Damian Moore <damian@epixstudios.co.uk>
2014-07-03 21:41:29 +0100

    Game modifications

--- team4/game.py
+++ team4/game.py
@@ -1,4 +1,15 @@
 import random
+from time import sleep
+
+
+def build_wordlist(file="/etc/dictionaries-common/words"):
+  words = []
+  with open(file) as word_file:
+    for line in word_file:
+      word = line.strip().lower()
+      if len(word) == 3 and "'" not in word:
+        words.append(word)
+  return words
 
 
 def dictionary():
@@ -14,10 +25,14 @@ class WinEvent(Exception):
 class Game(object):
 
     def __init__(self, secret=None):
-        self.secret = secret or random.choice(dictionary())
+        self.secret = secret or random.choice(build_wordlist())
+        self.guesses = 0
 
     def guess(self, word):
+        self.guesses += 1
+        sleep(0.2)
         matches = sum(a == b for a, b in zip(self.secret, word))
         if matches == len(self.secret):
+            print "It took", self.guesses, "guesses to guess", self.secret
             raise WinEvent("Well done, you won!")
         return matches
