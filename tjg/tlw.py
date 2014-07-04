8155ae00ed95be10a00907ebe165bf2bea7fd7f7
Tim Golden <mail@timgolden.me.uk>
2014-07-04 17:50:48 +0100

    Refactor slightly to allow for multiple techniques

--- tjg/tlw.py
+++ tjg/tlw.py
@@ -9,28 +9,39 @@ class Game(object):
 
     def __init__(self, length=3):
         self.unusable_words = set()
+
+        #
+        # Technique 1
+        #
         self.usable_words = set()
         self.length = int(length)
 
-        #~ self.definite = [set() for i in range(self.length)]
-        #~ self.possible = [set() for i in range(self.length)]
-        #~ self.impossible = [set() for i in range(self.length)]
+        #
+        # Technique 2
+        #
+        self.definite = [set() for i in range(self.length)]
+        self.possible = [set() for i in range(self.length)]
+        self.impossible = [set() for i in range(self.length)]
 
     @staticmethod
     def score(w1, w2):
         return sum(1 for i1, i2 in zip(w1, w2) if i1 == i2)
 
+    #
+    # Technique 1 is basically brute-force: select a word at random;
+    # if you get the full score, you've won; if you score zero, eliminate
+    # any word with any letters matching; otherwise, do nothing.
+    #
     def select_word1(self):
         return random.choice(list(self.usable_words))
 
-    select_word = select_word1
+    def update_stats1(self, word, score):
 
-    def discard_word(self, word):
-        self.usable_words.remove(word)
-        self.unusable_words.add(word)
+        def discard_word(w):
+            self.usable_words.remove(w)
+            self.unusable_words.add(w)
 
-    def update_stats(self, word, score):
-        self.discard_word(word)
+        discard_word(word)
         #
         # Remove any words with any of the letters in our zero-score
         # word.
@@ -38,9 +49,18 @@ class Game(object):
         if score == 0:
             for usable_word in set(self.usable_words):
                 if any(i1 == i2 for i1, i2 in zip(word, usable_word)):
-                    self.discard_word(usable_word)
+                    discard_word(usable_word)
 
         print("There are %d words left" % len(self.usable_words))
+
+    def select_word2(self):
+        raise NotImplementedError
+
+    def update_stats2(self, word, score):
+        raise NotImplementedError
+
+    select_word = select_word1
+    update_stats = update_stats1
 
     def run(self):
         self.usable_words = set(w.strip() for w in open("words.txt") if len(w) == 1 + self.length)
